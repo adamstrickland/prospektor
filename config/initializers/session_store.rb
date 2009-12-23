91c25843645077f132ee10ab89e6dd08e3c44fd3
Adam Strickland <adam.strickland@gmail.com>
2009-12-23 16:58:19 -0600

    rename project to prospektor

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_pursuit_session',
+  :key         => '_prospektor_session',
   :secret      => '7f868479f42268cd5f943906bf51589eeb0e25badfec9313e54359137464d5203f3437f9473b31a645c75e55f439cec3ec406760f76a26de1df2b935907b5b6d'
 }
 
