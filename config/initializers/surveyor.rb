f8be32812d4b5a3cc81852b81535435d45629c47
Adam Strickland <adam.strickland@gmail.com>
2010-02-10 17:37:25 -0600

    created custom surveyor layout

--- config/initializers/surveyor.rb
+++ config/initializers/surveyor.rb
@@ -1,11 +1,11 @@
 Surveyor::Config.run do |config|
   config['default.relative_url_root'] = nil # "surveys/" # should end with '/'
   config['default.title'] = nil # "You can take these surveys:"
-  config['default.layout'] = nil # "surveyor_default"
+  config['default.layout'] = 'surveyor' # "surveyor_default"
   config['default.index'] =  nil # "/surveys" # or :index_path_method
   config['default.finish'] =  nil # "/surveys" # or :finish_path_method
   config['use_restful_authentication'] = false # set to true to use restful authentication
-  config['extend_controller'] = false # set to true to extend SurveyorController
+  config['extend_controller'] = true # set to true to extend SurveyorController
 end
 
 # require 'models/survey_extensions' # Extended the survey model
