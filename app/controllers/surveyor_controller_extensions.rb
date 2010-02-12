9d03da1006dfc496e69448f1f2bb5891f585b446
Adam Strickland <adam.strickland@gmail.com>
2010-02-12 00:17:40 -0600

    basic surveyor extension setup

--- app/controllers/surveyor_controller_extensions.rb
+++ app/controllers/surveyor_controller_extensions.rb
@@ -1,10 +1,14 @@
+require File.join(Rails.root, 'vendor', 'plugins', 'surveyor', 'app', 'models', 'response_set')
+# require File.join(Rails.root, 'app', 'models', 'response_set')
+
 module SurveyorControllerExtensions
   def self.included(base)
     base.extend(ClassMethods)
     base.send(:include, InstanceMethods)
     base.class_eval do
       # Same as typing in the class
-      # before_filter :pimp_my_ride
+      skip_before_filter :login_required
+      before_filter :capture_lead_as_responder, :only => [:edit, :update]
     end
   end
   
@@ -12,17 +16,21 @@ module SurveyorControllerExtensions
   end
   
   module InstanceMethods
-    # def pimp_my_ride
-    #   flash[:notice] = "pimped!"
-    # end
+    protected
+    
+    def capture_lead_as_responder
+      rs = ResponseSet.find_by_access_code(params[:response_set_code])
+      unless rs.has_lead?
+        lead = Lead.find_by_key(params[:key])
+        if lead.present?
+          rs.lead = lead
+          rs.save
+        end
+      end
+    end
   end
   
   module Actions
     # Redefine the controller actions [index, new, create, show, update] here
-    # def new
-    #   render :text =>"surveys are down"
-    # end
   end
 end
-
-# Set config['extend_controller'] = true in config/initializers/surveyor.rb to activate these extensions
