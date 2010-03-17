c8e253e9d0b9eacbbc032e07c394d4e81900d39a
Adam Strickland <adam.strickland@gmail.com>
2010-03-16 20:12:46 -0500

    preso observer spec

--- app/models/presentation_observer.rb
+++ app/models/presentation_observer.rb
@@ -1,9 +1,9 @@
 class PresentationObserver < ActiveRecord::Observer
   def after_create(preso)
-    lead = preso.lead
+    # lead = preso.lead
     # lead.schedule
     # Rails.logger.warn("User for preso is:  #{preso.user.name}, User for lead is:  #{preso.lead.user.name}")
     ProspectMailer.deliver_presentation_invitation(preso)
-    lead.save!
+    # lead.save!
   end
 end
