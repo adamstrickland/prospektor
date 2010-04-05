1e5426ab433030a4a8cb3d0dc6773979a20f4e1b
Adam Strickland <adam.strickland@gmail.com>
2010-04-05 18:51:36 -0500

    restrict searching to some cols

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,9 @@ class SearchController < ApplicationController
   def index
     respond_to do |format|
       format.ajax do
+        search_columns = [:first_name, :last_name, :salutation, :title, :company, :address, :city, :state, :county, :zip, :phone]
         unless params[:search].blank?
-          @leads = Lead.paginate :page => params[:page] || 1, :per_page => 5, :conditions => Lead.conditions_by_like(params[:search])
+          @leads = Lead.paginate :page => params[:page] || 1, :per_page => 5, :conditions => Lead.conditions_by_like(params[:search], search_columns)
         end
         render :action => 'index', :layout => false
       end
