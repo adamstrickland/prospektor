249e973ba666295164f570140b8a02322699236e
Adam Strickland <adam.strickland@gmail.com>
2010-04-05 18:54:26 -0500

    moved search cols to model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,14 @@ class SearchController < ApplicationController
   def index
     respond_to do |format|
       format.ajax do
-        search_columns = [:first_name, :last_name, :salutation, :title, :company, :address, :city, :state, :county, :zip, :phone]
         unless params[:search].blank?
-          @leads = Lead.paginate :page => params[:page] || 1, :per_page => 5, :conditions => Lead.conditions_by_like(params[:search], search_columns)
+          # @leads = Lead.paginate :page => params[:page] || 1, :per_page => 5, :conditions => Lead.conditions_by_like(params[:search], search_columns)
+          options = {:page => params[:page] || 1, :per_page => 5}
+          if current_user.is_admin?
+            @leads = Lead.searchy(params[:search]).paginate(options)
+          else
+            @leads = current_user.leads.searchy(params[:search]).paginate(options)
+          end
         end
         render :action => 'index', :layout => false
       end
